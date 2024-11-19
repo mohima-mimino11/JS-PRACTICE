101deeb76f394344195dd2958980617f871901b7
mohima-mimino11 <mohimahoque22@gmail.com>
2024-11-19 22:09:51 +0600

    more array problems added

---
+++ reverse-3.js
@@ -0,0 +1,17 @@
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+// console.log(numbers);
+// numbers.reverse();
+// console.log(numbers);
+
+const rev_numbers = [];
+for(const num of numbers){
+  // console.log(num);
+  rev_numbers.unshift(num);
+  
+}
+// console.log(rev_numbers);
+
+
+
+
