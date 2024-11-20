8c279763d42b44034fd404ead310e478c951b68a
mohima-mimino11 <mohimahoque22@gmail.com>
2024-11-20 23:41:15 +0600

    More function practice files added

--- homework-function.js
+++ homework-function.js
@@ -46,3 +46,23 @@ const size = numbers.length;
 
 
 
+
+
+
+
+
+
+//Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+function odd_even(num){
+   if(num % 2 === 0){
+    return "Even";
+   }else{
+    return "Odd";
+   }
+}
+
+console.log(odd_even(8));
+console.log(odd_even(3));
+
+
