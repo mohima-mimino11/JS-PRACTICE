8b6ecb799dc7d21c3eeefef7af093eab97038826
mohima-mimino11 <mohimahoque22@gmail.com>
2024-11-25 00:07:56 +0600

    some more pratice problem added

---
+++ max.js
@@ -0,0 +1,17 @@
+const disha = 56;
+const salman = 95;
+
+function getMax(num1, num2){
+  if(num1 > num2){
+    return num1;
+  }else{
+    return num2;
+  }
+}
+
+
+const max1 = getMax(96, 79);
+const max2 = getMax(56, 98);
+const ultimateMax = getMax(max1, max2);
+
+console.log(`max of two is: ${ultimateMax}`);
